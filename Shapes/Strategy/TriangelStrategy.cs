6387dcdf1ac550164002b7e1447d83b58e572e8a
ElliotGisslen <elliot.gisslen@gmail.com>
2023-01-17 19:50:46 +0100

    b

--- Shapes/Strategy/TriangelStrategy.cs
+++ Shapes/Strategy/TriangelStrategy.cs
@@ -13,8 +13,8 @@ namespace Shapes.Strategy
             // Denna logik ska göras om för att funka med en triangel!!!
             // Just nu är det för rektangel :)
             var returnValues = new AreaPerimiter();
-            returnValues.Area = a * b;
-            returnValues.Perimiter = (a * 2) + (b * 2);
+            returnValues.Perimiter = a +b +c;
+            returnValues.Area = Math.Sqrt(returnValues.Perimiter/2*((returnValues.Perimiter / 2 - a)* (returnValues.Perimiter / 2 - b)* (returnValues.Perimiter / 2 - c)));
 
             return returnValues;
         }
